c670c96b60eaad0759ffaf9bf5a0a793d71e9bb0
Peter Williams <peter.williams@papercut.com>
2017-12-04 15:52:46 +1100

    added SpaCy tokenizer

--- rnn.py
+++ rnn.py
@@ -7,23 +7,31 @@ import re
 import random
 import time
 import os
+import spacy
+from collections import defaultdict
 
 
 # Parameters
-MAX_VOCAB = 40000
-SMALL_TEXT = True
+seed = 112
+use_spacy = True
+MAX_VOCAB = 10000
+MAX_WORDS = 10000
+SMALL_TEXT = False
 learning_rate = 0.001
 n_input = 3
 batch_size = 100
 train_frac = .1
-n_epochs = 10000
-patience = 100
+n_epochs = 1000
+n_epochs_report = 10
+patience = 20
 model_folder = 'models'
 
 # number of units in RNN cell
 n_hidden = 512
 
 UNKNOWN = '<UNKNOWN>'
+
+random.seed(seed)
 
 # Target log path
 logs_path = '/tmp/tensorflow/rnn_words'
@@ -71,17 +79,68 @@ RE_SPACE = re.compile(r'[\s,\.:;!\?\-\(\)]+', re.DOTALL | re.MULTILINE)
 RE_NUMBER = re.compile(r'[^A-Z^a-z]')
 
 
-def tokenize(text):
+def tokenize_simple(text, max_words):
     words = RE_SPACE.split(text)
     words = [w for w in words if len(w) > 1 and not RE_NUMBER.search(w)]
-    return words
+    if max_words > 0:
+        words = words[:max_words]
+    return [words]
 
 
-def build_indexes(words, MAX_VOCAB):
-    word_counts = {w: 0 for w in set(words)}
-    for w in words:
-        word_counts[w] += 1
-    vocabulary_list = sorted(word_counts, key=lambda x: (-word_counts[x], x))[:MAX_VOCAB - 1]
+spacy_nlp = spacy.load('en')
+punct = {',', '.', ';', ':', '\n', '\t', '\f'}
+
+
+def tokenize_spacy(text, n_input, max_words):
+    document = spacy_nlp(text)
+    doc_sents = list(document.sents)
+    print(len(doc_sents))
+    # for i, sent in enumerate(doc_sents[:20]):
+    #     print(i, type(sent), len(sent), sent[:5])
+    # sentences
+    sentences = []
+    n_words = 0
+    for span in document.sents:
+        sent = [token.text for token in span]
+        sent = [w for w in sent if w not in punct]
+        if len(sent) < n_input + 1:
+            continue
+        sentences.append(sent)
+        n_words += len(sent)
+        if max_words > 0 and n_words >= max_words:
+            break
+        # for token in sentence:
+        #     print(token)
+        #     token_dict = {}
+        #     token_dict['start'], token_dict['end'] = get_start_and_end_offset_of_token_from_spacy(token)
+        #     token_dict['text'] = text[token_dict['start']:token_dict['end']]
+        #     if token_dict['text'].strip() in ['\n', '\t', ' ', '']:
+        #         continue
+        #     # Make sure that the token text does not contain any space
+        #     if len(token_dict['text'].split(' ')) != 1:
+        #         print("WARNING: the text of the token contains space character, replaced with "
+        #               "hyphen\n\t{0}\n\t{1}".format(token_dict['text'], token_dict['text'].replace(' ', '-')))
+        #         token_dict['text'] = token_dict['text'].replace(' ', '-')
+        #     sentence_tokens.append(token_dict)
+        # sentences.append(sentence_tokens)
+    print('!!!', len(sentences))
+    for i, sent in enumerate(sentences[:5]):
+        print(i, type(sent), len(sent), sent[:5])
+    return sentences
+
+
+def tokenize(text, n_input, max_words):
+    if use_spacy:
+        return tokenize_spacy(text, n_input, max_words)
+    return tokenize_simple(text, max_words)
+
+
+def build_indexes(sentences, max_vocab):
+    word_counts = defaultdict(int)
+    for sent in sentences:
+        for w in sent:
+            word_counts[w] += 1
+    vocabulary_list = sorted(word_counts, key=lambda x: (-word_counts[x], x))[:max_vocab - 1]
     vocabulary_list.append(UNKNOWN)
     vocabulary = set(vocabulary_list)
     # for i, w in enumerate(words[:5]):
@@ -113,46 +172,52 @@ def build_embeddings(word_index):
 
 text = make_text()
 print('%7d bytes' % len(text))
-all_words = tokenize(text)
-print('%7d words' % len(all_words))
-word_index, index_word, unk_index = build_indexes(all_words, MAX_VOCAB)
+sentences = tokenize(text, n_input, MAX_WORDS)
+print('%7d sentences' % len(sentences))
+print('%7d words' % sum(len(sent) for sent in sentences))
+word_index, index_word, unk_index = build_indexes(sentences, MAX_VOCAB)
 vocab_size = len(word_index)
 print('%7d vocab' % vocab_size)
 embeddings, unk_embedding = build_embeddings(word_index)
-n_samples = len(all_words) - n_input - 2
+n_samples = sum((len(sent) - n_input) for sent in sentences)
 print('%7d samples' % n_samples)
 
 
-def batch_getter(words, n_input, batch_size):
+def batch_getter(sentences, n_input, batch_size):
     """Generator that returns x, y, oneh_y in `batch_size` batches
         phrase is a random phrase of length n_input + 1 from words
         x = indexes of first n_input words
         y = index of last word
         returns x, y, one hot encoding of y
     """
-    sequence_numbers = list(range(n_samples))
+    sequence_numbers = []
+    for i, sent in enumerate(sentences):
+        for j in range(len(sent) - n_input - 1):
+            sequence_numbers.append((i, j))
     random.shuffle(sequence_numbers)
 
-    for k in range(0, len(sequence_numbers), batch_size):
-        n = min(len(sequence_numbers) - k, batch_size)
+    for k0 in range(0, len(sequence_numbers), batch_size):
+        n = min(len(sequence_numbers) - k0, batch_size)
         # print('****', k, n, len(sequence_numbers))
         oneh_y = np.empty((n, vocab_size), dtype=np.float32)
         indexes_x = np.empty((n, n_input), dtype=int)
         indexes_y = np.empty((n), dtype=int)
 
-        for j in range(n):
-            i = sequence_numbers[j + k]
-            assert i < len(words) - n_input - 1, (i, len(words), n_input)
-            assert i + n_input < len(words), 'i=%d j=%d words=%d sequence_numbers n_input=%d' % (
+        for k in range(n):
+            i, j = sequence_numbers[k0 + k]
+            words = sentences[i]
+            assert j < len(words) - n_input - 1, (i, j, len(words), n_input)
+            assert j + n_input < len(words), 'i=%d j=%d words=%d sequence_numbers n_input=%d' % (
                 i, j, len(words), len(sequence_numbers), n_input)
-            phrase_x = words[i:i + n_input]
-            phrase_y = words[i + n_input]
+
+            phrase_x = words[j:j + n_input]
+            phrase_y = words[j + n_input]
             wx = [word_index.get(w, unk_index) for w in phrase_x]
             wy = word_index.get(phrase_y, unk_index)
 
-            indexes_x[j] = np.array(wx)
-            indexes_y[j] = np.array(wy)
-            oneh_y[j] = embeddings.get(phrase_y, unk_embedding)
+            indexes_x[k] = np.array(wx)
+            indexes_y[k] = np.array(wy)
+            oneh_y[k] = embeddings.get(phrase_y, unk_embedding)
 
         # show('indexes_x', indexes_y)
         yield indexes_x, indexes_y, oneh_y
@@ -229,7 +294,7 @@ with tf.Session() as session:
         accuracies = []
         acc_total = 0.0
         loss_total = 0.0
-        source = batch_getter(all_words, n_input, batch_size)
+        source = batch_getter(sentences, n_input, batch_size)
 
         # Process minibatches of size `batch_size`
         for step, (indexes_x, indexes_y, onehot_y) in enumerate(source):
@@ -256,7 +321,7 @@ with tf.Session() as session:
             print('epoch=%3d acc=%.2f' % (epoch + 1, 100.0 * acc_total))
 
         done = epoch > best_epoch + patience or epoch == n_epochs - 1
-        if (epoch + 1) % 100 == 0 or done:
+        if (epoch + 1) % n_epochs_report == 0 or done:
             print("epoch=%3d (best=%3d): Average Loss=%9.6f, Average Accuracy=%5.2f%%" %
                   (epoch + 1, best_epoch + 1, loss_total, 100.0 * acc_total))
 
